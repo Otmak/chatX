6e17e6121a68c228bfaaf71b9236a4a85e00dc67
Otma <jakelovz@gmail.com>
2019-05-01 21:14:34 -0700

    indent to mainjs

--- app/main.js
+++ app/main.js
@@ -6,23 +6,22 @@ let socket = require('socket.io');
 mainApp.use(express.static('public'));
 
 let server = mainApp.listen(9000, function (req, res) {
-	 console.log("it's Working Hommie!");
-  });
+	console.log("it's Working Hommie!");
+});
  
 let io = socket(server); 
 io.on('connection', function (socket){
-	     console.log('CONNECTED!',socket.id);
+	console.log('CONNECTED!',socket.id);
 
-	     socket.on('chat', function(msg){
-	     	console.log('YESSSSSSSSS '+ msg);
-	     	io.emit('chat', msg);
+	socket.on('chat', function(msg){
+	 console.log('YESSSSSSSSS '+ msg);
+	 io.emit('chat', msg);
+ });
 
-	     });
-
-	     socket.on('disconnect',function(){
-	 	    console.log('DISCONNECT!');
-	     });
+	socket.on('disconnect',function(){
+	 console.log('DISCONNECT!');
+ });
 
 
-   });
+});
 
