9ff433ad8c9a81f74e428ad1f2225067d2e436fa
Otma <jakelovz@gmail.com>
2019-05-01 21:11:36 -0700

    wkin & pasting text on DOM

--- app/public/index.js
+++ app/public/index.js
@@ -3,17 +3,18 @@
 let socket = io();
 let form = document.getElementById('m-form');
 let textInfo = document.getElementById('textInfo');
-const li = document.createElement('li');
+let textBody = document.getElementById('textBody');
+     
 
 form.addEventListener('submit',function (e) {
 	 e.preventDefault();
 	 socket.emit('chat', textInfo.value); 
      console.log(textInfo.value);
-     textInfo.value = '';
-	 
-	  
-     socket.on('chat', function (){
+     textInfo.value = '';   
+}); 
 
-        
-     })
-})
+socket.on('chat', function (msg){
+	 let li = document.createElement('li');
+	 textBody.appendChild(li)
+	 li.innerHTML = msg;
+});
