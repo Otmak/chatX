a69831c7c98a8ea86f5e2d401cf3a51dbb99f039
Otmak <jakelovz@gmail.com>
2019-05-02 22:36:51 -0700

    Update index.js

--- app/public/index.js
+++ app/public/index.js
@@ -4,14 +4,14 @@ let textInfo = document.getElementById('textInfo');
 let textBody = document.getElementById('textBody');
      
 form.addEventListener('submit',function (e) {
-	 e.preventDefault();
-	 socket.emit('chat', textInfo.value); 
+     e.preventDefault();
+     socket.emit('chat', textInfo.value); 
      console.log(textInfo.value);
      textInfo.value = '';   
 }); 
 
 socket.on('chat', function (msg){
-	 let li = document.createElement('li');
-	 textBody.appendChild(li)
-	 li.innerHTML = msg;
+    let li = document.createElement('li');
+    textBody.appendChild(li)
+    li.innerHTML = msg;
 });
