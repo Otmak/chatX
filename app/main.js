68a7562f84bcb1205480be3feab11426c580b4bd
Otma <jakelovz@gmail.com>
2019-05-26 22:05:24 -0700

    added time stamp to chat for future use

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ let socket = require('socket.io');
 mainApp.use(express.static('public'));
 
 let server = mainApp.listen(9000, function (req, res) {
-	console.log("it's Working Hommie!");
+	console.log("***CONNECTED*** Please use localHost: 9000");
 });
  
 let io = socket(server); 
@@ -13,7 +13,7 @@ io.on('connection', function (socket){
 	console.log('CONNECTED!',socket.id);
 
 	socket.on('chat', function(msg){
-	 console.log('MESSAGE: '+ msg);
+	 console.log(`MESSAGE: ${msg}`);
 	   io.emit('chat', msg);
  });
 
--- app/public/index.js
+++ app/public/index.js
@@ -2,16 +2,24 @@ let socket = io();
 let form = document.getElementById('m-form');
 let textInfo = document.getElementById('textInfo');
 let textBody = document.getElementById('textBody');
-     
+
+let d = new Date();
+let h = d.getHours();
+let m = d.getMinutes();
+let timeStamp = `${h}:${m}`;
+
 form.addEventListener('submit',function (e) {
 	 e.preventDefault();
 	 socket.emit('chat', textInfo.value); 
-     console.log(textInfo.value);
+     console.log(`${textInfo.value} ${timeStamp}`);
      textInfo.value = '';   
 }); 
 
 socket.on('chat', function (msg){
+	 let p = document.createElement('p');
 	 let li = document.createElement('li');
+	 textBody.appendChild(p);
 	 textBody.appendChild(li);
-	 li.innerHTML = msg;
+	 p.innerHTML =  msg;
+	 //li.innerHTML = timeStamp;
 });
