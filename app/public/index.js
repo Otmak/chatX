85066966456d11c3a65c75a1fc449fd0deeb88a7
Otmak <jakelovz@gmail.com>
2020-02-06 19:46:34 -0800

    added some style to body for text

--- app/public/index.js
+++ app/public/index.js
@@ -2,25 +2,29 @@ let socket = io();
 let form = document.getElementById('m-form');
 let textInfo = document.getElementById('textInfo');
 let textBody = document.getElementById('textBody');
-
+//Defining Time
 let d = new Date();
 let h = d.getHours();
 let m = d.getMinutes();
+let s = d.getSeconds()
 let timeStamp = `${h}:${m}`;
 
-form.addEventListener('submit',function (e) {
+form.addEventListener('submit',function (e) 
+{
 	 e.preventDefault();
 	 socket.emit('chat', textInfo.value); 
      console.log(`${textInfo.value} ${timeStamp}`);
      textInfo.value = '';   
 }); 
 
-socket.on('chat', function (msg){
+socket.on('chat', function (msg)
+{
 	 let p = document.createElement('p');
-	 let li = document.createElement('li');
+	 let t = document.createElement('small');
+	 t.setAttribute('title', `Posted at ${timeStamp}:${s}`);
+	 t.setAttribute('class', 'timeClass')
+	 p.textContent = msg;
+	 t.textContent = timeStamp;
 	 textBody.appendChild(p);
-	 textBody.appendChild(li);
-	 p.innerHTML =  msg;
-	 
-	 //li.innerHTML = timeStamp;
+	 p.appendChild(t)
 });
